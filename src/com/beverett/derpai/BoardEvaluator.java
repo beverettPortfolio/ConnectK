31738ae69ac78962e3b766915e00ffca197badbb
beverettPortfolio <ben.y.everett@gmail.com>
2016-09-15 20:22:23 -0700

    Refactored Evaluation function again

--- src/com/beverett/derpai/BoardEvaluator.java
+++ src/com/beverett/derpai/BoardEvaluator.java
@@ -1,7 +1,6 @@
 package com.beverett.derpai;
 
 import java.awt.Point;
-import java.util.HashMap;
 
 import connectK.BoardModel;
 
@@ -12,18 +11,20 @@ import connectK.BoardModel;
  *
  */
 public class BoardEvaluator {
-	private static enum Heuristics {
-		P1LINES, P2LINES, P1EVEN, P1ODD, P2EVEN, P2ODD
-	}
 
+	//The weights for the evaluation function.
+	private static final int WINNING_LINE_WEIGHT = 10;
+	private static final int EVEN_THREAT_WEIGHT = 1;
+	private static final int ODD_THREAT_WEIGHT = 1;
+	
 	/**
-	 * The evaluation function for the MiniMax search. The Heuristics are:
-	 * Winning lines: A line of k spaces in any direction that contains at least
-	 * one allied piece and no enemy pieces Even threats: A winning line with
-	 * k-1 allied pieces where the empty space is on an even row Odd threats: A
-	 * winning line with k-1 allied pieces where the empty space is on an odd
-	 * row Weights for the heuristics are set at the end When gravity is off,
-	 * even and odd threats are ignored.
+	 * The evaluation function for the MiniMax search.
+	 * The Heuristics are:
+	 * Winning lines: A line of k spaces in any direction that contains at least one allied piece and no enemy pieces 
+	 * Even threats: A winning line with k-1 allied pieces where the empty space is on an even row 
+	 * Odd threats: A winning line with k-1 allied pieces where the empty space is on an odd row 
+	 * Weights for the heuristics are set at the end 
+	 * When gravity is off, even and odd threats are ignored.
 	 * 
 	 * @param searchState
 	 *            The BoardModel to be evaluated
@@ -31,12 +32,7 @@ public class BoardEvaluator {
 	 *         numbers are more favorable for the active player
 	 */
 	public static int evaluate(BoardModel searchState, byte player) {
-		int p1Lines = 0;
-		int p2Lines = 0;
-		int p1Even = 0;
-		int p2Even = 0;
-		int p1Odd = 0;
-		int p2Odd = 0;
+		int score = 0;
 		byte winner = searchState.winner();
 		// No winner, evaluate
 		if (winner == -1) {
@@ -46,53 +42,23 @@ public class BoardEvaluator {
 			for (int i = 0; i < searchState.getWidth(); ++i) {
 				for (int j = 0; j < searchState.getHeight(); ++j) {
 					// Horizontal
-					HashMap<Heuristics, Integer> horizontalRows = searchRow(searchState, i, j, kLength, 1, 0);
-					p1Lines += horizontalRows.get(Heuristics.P1LINES);
-					p2Lines += horizontalRows.get(Heuristics.P2LINES);
-					p1Even += horizontalRows.get(Heuristics.P1EVEN);
-					p1Odd += horizontalRows.get(Heuristics.P1ODD);
-					p2Even += horizontalRows.get(Heuristics.P2EVEN);
-					p2Odd += horizontalRows.get(Heuristics.P2ODD);
+					score += searchRow(searchState, i, j, kLength, 1, 0);
 
 					// Vertical. Threats are not counted for vertical winning
 					// lines
-					HashMap<Heuristics, Integer> verticalRows = searchRow(searchState, i, j, kLength, 0, 1);
-					p1Lines += verticalRows.get(Heuristics.P1LINES);
-					p2Lines += verticalRows.get(Heuristics.P2LINES);
+					score += searchRow(searchState, i, j, kLength, 0, 1);
 
 					// Diagonal up and to the right
-					HashMap<Heuristics, Integer> upRightRows = searchRow(searchState, i, j, kLength, 1, 1);
-					p1Lines += upRightRows.get(Heuristics.P1LINES);
-					p2Lines += upRightRows.get(Heuristics.P2LINES);
-					p1Even += upRightRows.get(Heuristics.P1EVEN);
-					p1Odd += upRightRows.get(Heuristics.P1ODD);
-					p2Even += upRightRows.get(Heuristics.P2EVEN);
-					p2Odd += upRightRows.get(Heuristics.P2ODD);
+					score += searchRow(searchState, i, j, kLength, 1, 1);
 
 					// Diagonal up and to the left
-					HashMap<Heuristics, Integer> upLeftRows = searchRow(searchState, i, j, kLength, -1, 1);
-					p1Lines += upLeftRows.get(Heuristics.P1LINES);
-					p2Lines += upLeftRows.get(Heuristics.P2LINES);
-					p1Even += upLeftRows.get(Heuristics.P1EVEN);
-					p1Odd += upLeftRows.get(Heuristics.P1ODD);
-					p2Even += upLeftRows.get(Heuristics.P2EVEN);
-					p2Odd += upLeftRows.get(Heuristics.P2ODD);
+					score += searchRow(searchState, i, j, kLength, -1, 1);
 				}
 			}
-			// set weights here
-			int evaluationFunction;
-			// gravity on, weight even and odd threats
-			if (searchState.gravityEnabled()) {
-				evaluationFunction = (10 * p1Lines) - (10 * p2Lines) + (p1Even) + (p1Odd) - (p2Even) - (p2Odd);
-			}
-			// gravity off, only check winning lines
-			else {
-				evaluationFunction = (p1Lines) - (p2Lines);
-			}
 			if (player == 1) {
-				return evaluationFunction;
+				return score;
 			} else {
-				return -evaluationFunction;
+				return -score;
 			}
 		}
 		// If Either player wins return Infinity or -Infinity
@@ -126,16 +92,13 @@ public class BoardEvaluator {
 	 *            only. Downward movement is not needed.
 	 * @return A hashmap containing the Heuristics
 	 */
-	private static HashMap<Heuristics, Integer> searchRow(BoardModel searchState, int row, int column, int k, int horizontalMovement, int verticalMovement) {
+	private static int searchRow(BoardModel searchState, int row, int column, int k, int horizontalMovement, int verticalMovement) {
 		int countK = 0;
 		int count1 = 0;
 		int count2 = 0;
 		int count0 = 0;
-		HashMap<Heuristics, Integer> values = new HashMap<Heuristics, Integer>();
-		for (Heuristics h : Heuristics.values()) {
-			values.put(h, 0);
-		}
 		Point threat = null;
+		int score = 0;
 		// Make sure the line is within the board's boundaries
 		if (row + (k * horizontalMovement) >= -1 && row + (k * horizontalMovement) <= searchState.getWidth() && column + (k * verticalMovement) <= searchState.getHeight()) {
 			while ((count1 == 0 || count2 == 0) && countK < k) {
@@ -155,31 +118,31 @@ public class BoardEvaluator {
 			}
 			// Winning line for p1
 			if (count1 > 0 && count2 == 0) {
-				values.put(Heuristics.P1LINES, values.get(Heuristics.P1LINES) + 1);
+				score += WINNING_LINE_WEIGHT;
 				// Threats are checked for lines with horizontal movement when
 				// gravity is on
 				if (horizontalMovement != 0 && searchState.gravityEnabled() && count1 == k - 1 && count0 == 1) {
 					if (threat.getY() % 2 == 0) {
-						values.put(Heuristics.P1EVEN, values.get(Heuristics.P1EVEN) + 1);
+						score += EVEN_THREAT_WEIGHT;
 					} else {
-						values.put(Heuristics.P1ODD, values.get(Heuristics.P1ODD) + 1);
+						score += ODD_THREAT_WEIGHT;
 					}
 				}
 			}
 			// Winning line for p2
 			else if (count2 > 0 && count1 == 0) {
-				values.put(Heuristics.P2LINES, values.get(Heuristics.P2LINES) + 1);
+				score -= WINNING_LINE_WEIGHT;
 				// Threats are checked for lines with horizontal movement when
 				// gravity is on
 				if (horizontalMovement != 0 && searchState.gravityEnabled() && count2 == k - 1 && count0 == 1) {
 					if (threat.getY() % 2 == 0) {
-						values.put(Heuristics.P2EVEN, values.get(Heuristics.P2EVEN) + 1);
+						score -= EVEN_THREAT_WEIGHT;
 					} else {
-						values.put(Heuristics.P2ODD, values.get(Heuristics.P2ODD) + 1);
+						score -= ODD_THREAT_WEIGHT;
 					}
 				}
 			}
 		}
-		return values;
+		return score;
 	}
 }
--- src/test/EvaluatorTest.java
+++ src/test/EvaluatorTest.java
@@ -15,8 +15,8 @@ public class EvaluatorTest extends AITest {
 	public static TestSuite suite() {
 		TestSuite testSuite = new TestSuite();
 		testSuite.addTest(new EvaluatorTest("Empty Board", new byte[][] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } }, 3, false, 0));
-		testSuite.addTest(new EvaluatorTest("Center", new byte[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } }, 3, false, 4));
-		testSuite.addTest(new EvaluatorTest("1 Corner 2 Center", new byte[][] { { 1, 0, 0 }, { 0, 2, 0 }, { 0, 0, 0 } }, 3, false, -1));
+		testSuite.addTest(new EvaluatorTest("Center", new byte[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } }, 3, false, 40));
+		testSuite.addTest(new EvaluatorTest("1 Corner 2 Center", new byte[][] { { 1, 0, 0 }, { 0, 2, 0 }, { 0, 0, 0 } }, 3, false, -10));
 		testSuite.addTest(new EvaluatorTest("Empty gravity", new byte[][] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } }, 3, true, 0));
 		testSuite.addTest(new EvaluatorTest("Center gravity", new byte[][] { { 0, 0, 0 }, { 0, 0, 1 }, { 0, 0, 0 } }, 3, true, 20));
 		testSuite.addTest(new EvaluatorTest("Corner gravity", new byte[][] { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } }, 3, true, 30));
